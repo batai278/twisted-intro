95ca3e76441943bea4ceafd283425a4079bf3848
Illia Yurtsiv <illiainsomathy@gmail.com>
2018-01-05 19:17:23 +0200

    problems with importing global arg_parsing fixed

--- __init__.py
+++ __init__.py
@@ -1,3 +1,8 @@
-import sys
-sys.path.append('C:\\Users\\PC\\Source\\Repos\\')
-sys.path.append('C:\\Users\\PC\\Source\\Repos\\twisted_intro')
+#import sys
+#sys.path.append('C:\\Users\\PC\\Source\\Repos\\')
+#sys.path.append('C:\\Users\\PC\\Source\\Repos\\twisted_intro')
+import argparse
+import arg_parsing
+from arg_parsing import parse_args
+
+__all__ = ['arg_parsing','parse_args']
